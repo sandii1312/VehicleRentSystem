3c423cac960b8b553362129ceece1cd51bb0c4b7
sandii1312 <mr.santhosh13@gmail.com>
2024-01-18 19:59:42 +0530

    Admin functionality updated

--- src/Main.java
+++ src/Main.java
@@ -13,8 +13,56 @@ public class Main {
                 String role = ser.signIn();
                 if (role.equals("admin")) {
                     System.out.println("WELCOME ADMIN");
+                    while(true){
+                        System.out.println("1.List all vehicle\n2.Search a Vehicle\n3.Add a vehicle\n4.Modify Vehicle Specification\n5.Vehicle Need to service\n6.logout");
+                        int adminUse=scan.nextInt();
+                        scan.nextLine();
+                        if(adminUse==1){
+                            System.out.println("***************CARS LIST********************");
+                            ser.showVehicleCar();
+                            System.out.println("***************BIKES LIST********************");
+                            ser.showVehicleBike();
+                        } else if (adminUse==2) {
+                            System.out.println("1.Search Vehicle by Name\n2. search Vehicle by number\n3. Back");
+                            int search= scan.nextInt();
+                            scan.nextLine();
+                            if(search==1){
+                                System.out.println("Model name");
+                                String vehicleName= scan.nextLine();
+                                ser.searchByName(vehicleName);
+                            } else if (search==2) {
+                                System.out.println("Vehicle number");
+                                String vehicleNumber= scan.nextLine();
+                                ser.searchByNumber(vehicleNumber);
+                            } else if (search==3) {
+                                continue;
+                            }
+
+                        } else if (adminUse==3) {
+                            int res=ser.addVehicle();
+                            if(res==1){
+                                System.out.println("Vehicle added Successfully");
+                            }
+                            else {
+                                System.out.println("Something went wrong");
+                            }
+                        } else if (adminUse==4) {
+                            System.out.println("1.Delete Vehicle\n2. back");
+                            int operation= scan.nextInt();
+                            scan.nextLine();
+                            if(operation==1){
+                                System.out.println("Vehicle Number");
+                                ser.deleteVehicle(scan.nextLine());
+                            }
+                        } else if (adminUse==5) {
+                            ser.vehicleNeedToService();
+                        }else if(adminUse==6){
+                            break;
+                        }
+                    }
                 } else if(role.equals("customer")){
                     System.out.println("WELCOME USER");
+
                 }
                 else if(role.equals("USER NOT FOUND")){
                     System.out.println("Sign up to continue");
--- src/server.java
+++ src/server.java
@@ -1,3 +1,4 @@
+import java.security.spec.ECField;
 import java.sql.*;
 import java.util.Scanner;
 
@@ -13,7 +14,7 @@ public class server{
             st= con.createStatement();
         }
         catch (Exception e){
-            System.out.println("CLASS NOT FOUNT");
+            System.out.println("CLASS NOT FOUND");
         }
     }
 
@@ -62,9 +63,170 @@ public class server{
         catch (Exception e){
             return -1;
         }
+    }
+    public int addVehicle(){
+        Vehicle data=new Vehicle();
+        System.out.println("Brand :");
+        data.setBrand(scan.nextLine());
+        System.out.println("Model Name :");
+        data.setModelName(scan.nextLine());
+        System.out.println("category :");
+        data.setCategory(scan.nextLine());
+        System.out.println("Vehicle Type(car/bike) :");
+        data.setType(scan.nextLine().toLowerCase());
+        System.out.println("Vehicle Number Plate :");
+        data.setNumberPlate(scan.nextLine().replace(" ",""));
+        System.out.println("Kilometre Driven :");
+        data.setKmDriven(scan.nextInt());
+        if(data.getType().equals("car")){
+            data.setNextServices(data.getKmDriven()+3000);
+        }
+        else {
+            data.setNextServices(data.getKmDriven()+1500);
+        }
+        data.setAvailableStatus(true);
+        System.out.println("Vehicle Deposit value");
+        data.setVehicleDeposit(scan.nextInt());
+        System.out.println("Rent per day");
+        data.setRentPerDay(scan.nextInt());
+        try {
+            String query = "INSERT INTO VEHICLE (brand, modelName, category, type, numberPlate, kmDriven, nextService, availableStatus, vehicleDeposit, rentPerDay) " +
+                    "VALUES (?,?,?,?,?,?,?,?,?,?)";
+            PreparedStatement st = con.prepareStatement(query);
+            st.setString(1, data.getBrand());
+            st.setString(2, data.getModelName());
+            st.setString(3, data.getCategory());
+            st.setString(4, data.getType());
+            st.setString(5, data.getNumberPlate());
+            st.setInt(6, data.getKmDriven());
+            st.setInt(7, data.getNextServices());
+            st.setBoolean(8, data.isAvailableStatus());
+            st.setInt(9, data.getVehicleDeposit());
+            st.setInt(10,data.getRentPerDay());
 
+            int count = st.executeUpdate();
 
+            return count;
+
+        }catch (Exception e){
+            System.out.println(e);
+            return 0;
+        }
     }
 
+    public void showVehicleCar(){
+        try {
+            String query = "SELECT * FROM VEHICLE WHERE TYPE = 'CAR';";
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            boolean flag=false;
+            System.out.println("-----------------------------------------------------------------------------------------------------------------");
+            while (rs.next()) {
+                System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3)
+                        + " " + rs.getString(4) + " " + rs.getString(5) + " " + rs.getString(6)
+                        + " " + rs.getString(7) + " " + rs.getString(8) + " " + rs.getString(9) + " " + rs.getString(10));
+                System.out.println("-----------------------------------------------------------------------------------------------------------------");
+            }
+            if(!flag){
+                System.out.println("No Vehicle found");
+                System.out.println("-----------------------------------------------------------------------------------------------------------------");
+            }
+        }catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+    }
+    public void showVehicleBike(){
+        try {
+            String query = "SELECT * FROM VEHICLE WHERE TYPE = 'BIKE';";
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            boolean flag=false;
+            System.out.println("-----------------------------------------------------------------------------------------------------------------");
+            while (rs.next()) {
+                flag=true;
+                System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3)
+                        + " " + rs.getString(4) + " " + rs.getString(5) + " " + rs.getString(6)
+                        + " " + rs.getString(7) + " " + rs.getString(8) + " " + rs.getString(9) + " " + rs.getString(10));
+                System.out.println("-----------------------------------------------------------------------------------------------------------------");
+            }
+            if(!flag){
+                System.out.println("No Vehicle found");
+                System.out.println("-----------------------------------------------------------------------------------------------------------------");
+            }
+        }catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+    }
 
+    public void searchByName(String name) {
+        try {
+            String query = "SELECT * FROM vehicle where modelName='"+name+"';";
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            System.out.println("-----------------------------------------------------------------------------------------------------------------");
+            while (rs.next()) {
+                System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3)
+                        + " " + rs.getString(4) + " " + rs.getString(5) + " " + rs.getString(6)
+                        + " " + rs.getString(7) + " " + rs.getString(8) + " " + rs.getString(9) + " " + rs.getString(10));
+                System.out.println("-----------------------------------------------------------------------------------------------------------------");
+            }
+        }catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+
+    }
+    public void searchByNumber(String num) {
+        try {
+            String query = "SELECT * FROM vehicle where numberPlate = '" + num + "';";
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            System.out.println("-----------------------------------------------------------------------------------------------------------------");
+            while (rs.next()) {
+                System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3)
+                        + " " + rs.getString(4) + " " + rs.getString(5) + " " + rs.getString(6)
+                        + " " + rs.getString(7) + " " + rs.getString(8) + " " + rs.getString(9) + " " + rs.getString(10));
+                System.out.println("-----------------------------------------------------------------------------------------------------------------");
+            }
+        }catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+    }
+
+    public void vehicleNeedToService(){
+        try {
+            String query = "SELECT * FROM vehicle where kmDriven >= nextService";
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            System.out.println("-----------------------------------------------------------------------------------------------------------------");
+            if(!rs.next()) {
+                System.out.println("NO vehicle need to service");
+                System.out.println("-----------------------------------------------------------------------------------------------------------------");
+            }
+            while (rs.next()) {
+                System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3)
+                        + " " + rs.getString(4) + " " + rs.getString(5) + " " + rs.getString(6)
+                        + " " + rs.getString(7) + " " + rs.getString(8) + " " + rs.getString(9) + " " + rs.getString(10));
+                System.out.println("-----------------------------------------------------------------------------------------------------------------");
+            }
+        }catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+    }
+
+    public void deleteVehicle(String num){
+        try {
+            String query = "DELETE FROM vehicle WHERE numberPlate = '" + num + "';";
+            Statement st = con.createStatement();
+            int count = st.executeUpdate(query);
+            if(count==1){
+                System.out.println("Vehicle deleted successfully");
+            }
+            else{
+                System.out.println("Vehicle not found");
+            }
+        }catch (Exception e){
+            System.out.println("Error! Vehicle not found");
+            System.out.println(e);
+        }
+    }
 }
